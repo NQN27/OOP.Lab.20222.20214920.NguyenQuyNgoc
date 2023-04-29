739b93e95bd16e69bf1a6e0ff1d147d7e82f0460
NQN27 <104126045+NQN27@users.noreply.github.com>
2023-04-29 23:17:52 +0700

    lab02

---
+++ Lab_02/ReadingAssignment/AimsProject/src/aims/DigitalVideoDisc.java
@@ -0,0 +1,55 @@
+package aims;
+
+public class DigitalVideoDisc {
+	private String title = null;
+	private String category = null;
+	private String director = null;
+	private int length = 0;
+	private float cost = 0;
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	
+
+}
+
