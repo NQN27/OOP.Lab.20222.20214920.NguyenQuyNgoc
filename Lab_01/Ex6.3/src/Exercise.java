25c1fd4e4f7a9b13a8cb573d3923a8fae6e6a3ef
NQN27 <104126045+NQN27@users.noreply.github.com>
2023-04-30 18:24:29 +0700

    Update Exercise.java

--- Lab_01/Ex6.3/src/Exercise.java
+++ Lab_01/Ex6.3/src/Exercise.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 public class Exercise {
 
 	public static void main(String[] args) {
-		System.out.println("Input the number of row you want");
+		int n =0;
+		boolean check = true;
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
+		while (check == true) {
+		try {
+		System.out.println("Input the number of row you want");
+		n = scanner.nextInt();
+		if (n<1) {
+			check= true;
+			System.out.println("Input number higher or equal to 1");}
+		else { check = false;}
+		} catch (Exception e) {
+			check = true;
+			System.out.println("wrong type input");
+			scanner.nextLine();
+			}
+		}
 		for (int i=1;i<=n;i++) {
 			String a ="";
 			String b ="";
